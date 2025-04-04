281d43c65a3c0aa1c994f3997c9b9d1a72778bdf
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-04-04 10:00:24 -0600

    refactor: Change the main banner code to a separate file

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,20 +1,10 @@
-import PrimaryButton from "../components/commons/buttons/PrimaryButton";
+import MainBanner from "../components/banners/MainBanner";
 
 const App = () => {
   return (
-    <section>
-      <h1 className="text-primary-black font-bold text-center leading-7 ssm:text-[1.7rem]">
-        un sitio dedicado a la programación y a las nuevas tecnologías
-      </h1>
-      <p className="font-normal mt-3 text-gray-light text-center leading-5">
-        Explora tendencias tecnológicas, aprende sobre programación y comparte
-        tus propios artículos con la comunidad
-      </p>
-      <div className="flex gap-2 mt-2 justify-center">
-        <PrimaryButton title="Explorar" />
-        <PrimaryButton title="Comienza ahora" />
-      </div>
-    </section>
+    <div>
+      <MainBanner />
+    </div>
   );
 };
 

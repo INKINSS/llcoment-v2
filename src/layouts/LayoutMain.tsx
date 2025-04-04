9609fb87e61ecbc2a12a22dd44aaadb252774982
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-04-04 10:38:35 -0600

    feat(layout): adds new navigation menu

--- src/layouts/LayoutMain.tsx
+++ src/layouts/LayoutMain.tsx
@@ -1,11 +1,15 @@
 import { Outlet } from "react-router-dom";
 import { DotPattern } from "../components/magicui/dot-pattern";
+import MainNavBar from "../components/navBars/MainNavBar";
 
 const LayoutMain = () => {
   return (
-    <main className="flex px-6 justify-center items-center h-screen">
-      <Outlet />
-      <DotPattern className="fixed -z-20" />
+    <main className="flex flex-col px-6 h-screen">
+      <MainNavBar />
+      <div className="flex flex-col justify-center items-center h-full">
+        <Outlet />
+        <DotPattern className="fixed -z-20" />
+      </div>
     </main>
   );
 };

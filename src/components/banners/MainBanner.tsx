ca3f618da7ab04ac7b6e4f03062b2d808586a223
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-04-06 20:17:37 -0600

    style(responsive): addd new class for responsive themes in main banner

--- src/components/banners/MainBanner.tsx
+++ src/components/banners/MainBanner.tsx
@@ -1,21 +1,24 @@
-import PrimaryButton from '../commons/buttons/PrimaryButton'
+import PrimaryButton from "../commons/buttons/PrimaryButton";
+import { BlurFade } from "../magicui/blur-fade";
 
 const MainBanner = () => {
   return (
-    <section className='md:w-[70%]'>
-      <h1 className="text-primary-black font-bold text-center leading-7 ssm:text-[1.7rem]">
-        un sitio dedicado a la programación y a las nuevas tecnologías
-      </h1>
-      <p className="font-normal mt-3 text-gray-light text-center leading-5">
-        Explora tendencias tecnológicas, aprende sobre programación y comparte
-        tus propios artículos con la comunidad
-      </p>
-      <div className="flex gap-2 mt-4 justify-center">
-        <PrimaryButton title="Explorar" />
-        <PrimaryButton title="Comienza ahora" />
-      </div>
+    <section className="md:w-[70%]">
+      <BlurFade>
+        <h1 className="text-primary-black font-bold text-center leading-9 ssm:text-[1.7rem] md:text-[2rem] lg:text-[2.3rem]">
+          un sitio dedicado a la programación y a las nuevas tecnologías
+        </h1>
+        <p className="font-normal mt-3 text-gray-light text-center leading-6 md:text-[1.2rem] lg:text-[1.3rem]">
+          Explora tendencias tecnológicas, aprende sobre programación y comparte
+          tus propios artículos con la comunidad
+        </p>
+        <div className="flex gap-2 mt-4 justify-center">
+          <PrimaryButton title="Explorar" />
+          <PrimaryButton title="Comienza ahora" />
+        </div>
+      </BlurFade>
     </section>
-  )
-}
+  );
+};
 
-export default MainBanner
+export default MainBanner;

cc100bf0847915213d2d956c03a67bcacfdff6f4
Martin Samuel Perez Montenegro <martinsamuelpp@gmail.com>
2025-04-21 09:14:32 -0600

    feat: Add a dynamic page for each article using its slug

---
+++ src/pages/ArticleBySlug.tsx
@@ -0,0 +1,40 @@
+import { useParams } from "react-router-dom";
+import { useState, useEffect } from "react";
+import axios from 'axios'
+
+interface Article {
+    id: number;
+    title: string;
+    content: string;
+    tag: string;
+    nickname: string;
+    createdAt: string;
+    updatedAt: string;
+}
+
+const ArticleBySlug = () => {
+
+    const { slug } = useParams()
+    const [article, setArticle] = useState<Article | null>(null)
+
+    useEffect(() => {
+        const fetchArticle = async () => {
+            const response = await axios.get(`http://localhost:3000/api/article/${slug}`)
+            setArticle(response.data)
+        }
+        fetchArticle()
+    }, [slug])
+
+    if (!article) {
+        return <div>Loading...</div>
+    }
+
+  return (
+    <main>
+        <h2>{article.title}</h2>
+        <div dangerouslySetInnerHTML={{ __html: article.content }} ></div>
+    </main>
+  );
+};
+
+export default ArticleBySlug;
